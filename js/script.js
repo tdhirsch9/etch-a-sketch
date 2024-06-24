65be247a9fafacdb127c5a2595e2235b0e914536
Ted Hirsch <tdhirsch9@gmail.com>
2024-06-24 16:15:01 -0600

    Updated some basic code

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+
+const container = document.getElementById("container")
+
+const divOne = document.createElement("div")
+container.appendChild(divOne)
+divOne.style.color = "green"
+
+
+const divTwo = document.createElement("div")
+
+const divThree = document.createElement("div")
+
+const divFour = document.createElement("div")
+
+const divFive = document.createElement("div")
+
+const divSix = document.createElement("div")
+
+const divSeven = document.createElement("div")
+
+const divEight = document.createElement("div")
+
+const divNine = document.createElement("div")
