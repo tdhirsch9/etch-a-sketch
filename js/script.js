cd329145e9107b4ad1d2e2abdc0a2aac84b366a7
Ted Hirsch <tdhirsch9@gmail.com>
2024-06-27 14:21:46 -0600

    Finished Project

--- js/script.js
+++ js/script.js
@@ -1,25 +1,79 @@
+const sketcher = document.querySelector('#container');
+const defaultSize = 16;
+
+const resetBtn = document.getElementById("reset")
+
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 
 
-const container = document.getElementById("container")
-
-const divOne = document.createElement("div")
-container.appendChild(divOne)
-divOne.style.color = "green"
 
 
-const divTwo = document.createElement("div")
 
-const divThree = document.createElement("div")
 
-const divFour = document.createElement("div")
+const newGrid = () => {
 
-const divFive = document.createElement("div")
+    const size = prompt("How large would you like the grid to be?")
+    if (size >= 1 && size <= 100){
+        resetSketch()
+        createNewGrid(size)
+    } else{
+        alert("Please input a number between 1 and 100")
+    }
+}
 
-const divSix = document.createElement("div")
+function resetSketch() {
+    let squares = sketcher.lastElementChild
+    while (squares){
+        sketcher.removeChild(squares)
+        squares = sketcher.lastElementChild
+    }
+}
 
-const divSeven = document.createElement("div")
+resetBtn.addEventListener("click", newGrid)
 
-const divEight = document.createElement("div")
+function createNewGrid(size){
 
-const divNine = document.createElement("div")
+    const gridSize = 960 / size;
+
+
+    for (let i = 0; i < size * size; i++){
+        const newDiv = document.createElement("div");
+        newDiv.classList.add('grid-item');
+        newDiv.style['height'] = `${gridSize}px`;
+        newDiv.style['width'] = `${gridSize}px`;
+        newDiv.style.backgroundColor = "lightgrey"
+        
+        sketcher.appendChild(newDiv);
+
+    newDiv.addEventListener("mouseover", function(){
+        let a = "#"
+
+        for(i = 0; i < 6; i++){
+          a += array[randomNumber()]
+        }
+        
+       
+          
+            
+            
+        
+        newDiv.style.backgroundColor = a
+    });
+    
+        
+    }
+
+    
+
+
+
+}
+
+function randomNumber(){
+return random = Math.floor(Math.random() * array.length)
+}
+
+window.addEventListener("load", () => {
+    createNewGrid(defaultSize);
+  });
