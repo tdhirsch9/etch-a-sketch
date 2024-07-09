d45581da885c64bd06c1b28175d384767e1feae7
Ted Hirsch <tdhirsch9@gmail.com>
2024-07-09 16:16:48 -0600

    Improved CSS and Javascript code

--- js/script.js
+++ js/script.js
@@ -1,79 +1,66 @@
-const sketcher = document.querySelector('#container');
-const defaultSize = 16;
+const gridContainer = document.querySelector(".grid-container")
+const newGridButton = document.querySelector(".new-grid-button")
 
-const resetBtn = document.getElementById("reset")
+const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
-const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-
-
-
-
+document.addEventListener("DOMContentLoaded", function(){
+    makeGrid(16)
+})
 
 
+function getRandomColor() {
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    return randomColor;
+  }
 
-const newGrid = () => {
 
-    const size = prompt("How large would you like the grid to be?")
-    if (size >= 1 && size <= 100){
-        resetSketch()
-        createNewGrid(size)
+
+function gridReset(){
+    let squares = gridContainer.querySelectorAll(".row")
+    squares.forEach(squares => {
+        gridContainer.removeChild(squares);
+    })
+}
+
+newGridButton.addEventListener("click", function(){
+   let gridSizeRequest = prompt("What size grid would you like?")
+
+    if(gridSizeRequest > 0 && gridSizeRequest < 101){
+        gridReset()
+        makeGrid(gridSizeRequest)
     } else{
-        alert("Please input a number between 1 and 100")
+        alert("Please select a number from 1 to 100")
     }
-}
 
-function resetSketch() {
-    let squares = sketcher.lastElementChild
-    while (squares){
-        sketcher.removeChild(squares)
-        squares = sketcher.lastElementChild
-    }
-}
+   
+})
 
-resetBtn.addEventListener("click", newGrid)
+function makeGrid(e){
 
-function createNewGrid(size){
+    for (i = 0; i < e; i++){
+ 
+        let row = document.createElement("div")
+        gridContainer.appendChild(row)
+        row.classList.add("row")
+        for(let a = 0; a < e; a++){
+            let square = document.createElement("div")
+            row.appendChild(square)
+            square.classList.add("grid-square")
 
-    const gridSize = 960 / size;
-
-
-    for (let i = 0; i < size * size; i++){
-        const newDiv = document.createElement("div");
-        newDiv.classList.add('grid-item');
-        newDiv.style['height'] = `${gridSize}px`;
-        newDiv.style['width'] = `${gridSize}px`;
-        newDiv.style.backgroundColor = "lightgrey"
-        
-        sketcher.appendChild(newDiv);
-
-    newDiv.addEventListener("mouseover", function(){
-        let a = "#"
-
-        for(i = 0; i < 6; i++){
-          a += array[randomNumber()]
+        square.addEventListener("mouseover", function(){
+            let newColor = "#"
+            for(let i = 0; i < 6; i++){
+              
+               newColor += getRandomColor()
+                
+                console.log(newColor)
+            }
+        square.style.backgroundColor = newColor
+    })
         }
         
-       
-          
-            
-            
-        
-        newDiv.style.backgroundColor = a
-    });
-    
-        
+
     }
 
-    
-
-
 
 }
-
-function randomNumber(){
-return random = Math.floor(Math.random() * array.length)
-}
-
-window.addEventListener("load", () => {
-    createNewGrid(defaultSize);
-  });
